a79583cc5196078b8bc556c165888bcb696bd0e5
Lahoucine Aboulhassan <em.aboulhassan@gmail.com>
2021-01-21 16:58:47 +0000

    create Main component'

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <main v-bind:class="theme">
+    <Products v-bind:isLight="isLight" />
+    <About />
+  </main>
+</template>
+<script>
+import Products from './main/Products';
+import About from './main/About';
+export default {
+  name: 'Main',
+  components: {
+    Products,
+    About,
+  },
+  props: {
+    isLight: Boolean,
+    themeClass: String,
+  },
+  computed: {
+    theme: function() {
+      return this.isLight ? 'lighter container' : 'dark container';
+    },
+  },
+};
+</script>
+<style scoped></style>
