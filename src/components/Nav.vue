1be2c4033e45b51b4a1b603a0cb2c1e6074933c5
Lahoucine Aboulhassan <em.aboulhassan@gmail.com>
2021-01-21 16:54:45 +0000

    create nav bar components

---
+++ src/components/Nav.vue
@@ -0,0 +1,129 @@
+<template>
+  <nav>
+    <div v-bind:class="themeClass" class="topnav" id="myTopnav">
+      <div class="logo"><span>clothing </span>App</div>
+      <div class="menu">
+        <NavItem
+          v-for="link in links"
+          v-bind:key="link.link"
+          v-bind:link="link"
+          v-bind:themeClass="themeClass"
+        />
+        <div href="#" class="navBtn" v-on:click="display()">
+          <i class="bars icon responsive-btn"></i>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+import NavItem from './nav/NavItem';
+export default {
+  name: 'Nav',
+  components: {
+    NavItem,
+  },
+  props: {
+    themeClass: String,
+  },
+  data() {
+    return {
+      links: [
+        { link: '#home', value: 'home' },
+        { link: '#our-products', value: 'our products' },
+        { link: '#about-us', value: 'about us' },
+        { link: '#useful-links', value: 'useful links' },
+        { link: '#contact-us', value: 'contact us' },
+      ],
+    };
+  },
+  methods: {
+    display: function() {
+      // get the DOM items
+      const topNav = document.querySelector('.topnav');
+      const responsiveBtn = document.querySelector('.responsive-btn');
+      topNav.classList.toggle('responsive');
+      responsiveBtn.classList.toggle('times');
+      responsiveBtn.classList.toggle('bars');
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Custom Style */
+nav {
+  color: var(--light);
+  padding: 0 1rem;
+}
+.topnav {
+  width: 100%;
+  height: 56.8px;
+  text-transform: uppercase;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 2;
+  overflow: hidden;
+}
+.menu {
+  float: right;
+  text-transform: capitalize;
+}
+.logo {
+  float: left;
+  cursor: default;
+  font-size: 1.2rem;
+  font-weight: 600;
+  padding: 0.9rem;
+}
+.logo span {
+  color: var(--primary);
+}
+.navBtn {
+  display: none;
+}
+.navBtn:hover {
+  background: none;
+  color: var(--primary);
+}
+/* Screen styles */
+@media screen and (max-width: 768px) {
+  .topnav {
+    padding-top: 0.5rem;
+  }
+  .topnav .logo {
+    text-align: center;
+    padding: 0.7rem;
+    float: none;
+  }
+  .topnav .navBtn {
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 0.35rem;
+    padding: 0.9rem;
+  }
+  .topnav .menu .nav-item:hover {
+    border: none;
+  }
+  .responsive .menu .nav-item {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .responsive {
+    height: auto;
+    z-index: 2;
+  }
+  .responsive .menu {
+    float: none;
+  }
+  .responsive .active {
+    border: none;
+    background: var(--lightTransparence);
+    color: var(--primary);
+  }
+}
+</style>
---
+++ src/components/nav/NavItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <a
+    :class="link.link === '#home' ? themeClass + ' active' : themeClass"
+    class="nav-item light"
+    v-bind:href="link.link"
+    v-on:click="isActive"
+  >
+    {{ link.value }}
+  </a>
+</template>
+<script>
+export default {
+  name: 'NavItem',
+  props: {
+    link: Object,
+    themeClass: String,
+  },
+  methods: {
+    isActive: function(event) {
+      const navLinks = document.querySelectorAll('.nav-item');
+      console.log(navLinks);
+      navLinks.forEach((elem) => {
+        if (elem === event.target) {
+          event.target.classList.add('active');
+        } else elem.classList.remove('active');
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+.nav-item {
+  float: left;
+  cursor: pointer;
+  background: none;
+  text-align: center;
+  text-decoration: none;
+  font-weight: 600;
+  font-family: 'News Cycle', sans-serif;
+  padding: 0.9rem;
+}
+.nav-item:hover {
+  color: var(--primary);
+}
+.nav-item:focus {
+  outline: none;
+}
+.nav-item:visited {
+  color: inherit;
+}
+.nav-item.active {
+  border-bottom: 4px solid var(--primary);
+}
+</style>
